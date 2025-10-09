aa67e10b9e1b9246fa8571f060eca6355946e919
minjun <mj4863@naver.com>
2025-10-09 23:56:02 +0900

    login Example Value 수정

--- src/main/java/com/devops3sogang/backend/dto/LoginRequest.java
+++ src/main/java/com/devops3sogang/backend/dto/LoginRequest.java
@@ -8,12 +8,12 @@ import jakarta.validation.constraints.*;
 @Schema(description = "로그인 요청 DTO")
 public class LoginRequest {
 
-    @Schema(description = "로그인 이메일", example = "user@sogang.ac.kr")
+    @Schema(description = "로그인 이메일", example = "testuser1@sogang.ac.kr")
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
-    @Schema(description = "비밀번호", example = "password1234")
+    @Schema(description = "비밀번호", example = "mytestpassword1")
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
 }
