05d17eac6981fde7fca8e95e2fabd3f52ecff832
minjun <mj4863@naver.com>
2025-10-11 02:22:35 +0900

    fix: add userUpdate example value

--- src/main/java/com/devops3sogang/backend/dto/UserUpdateRequest.java
+++ src/main/java/com/devops3sogang/backend/dto/UserUpdateRequest.java
@@ -8,10 +8,10 @@ import lombok.Data;
 @Schema(description = "사용자 정보 수정 요청 DTO")
 public class UserUpdateRequest {
 
-    @Schema(description = "새 닉네임 (변경 원할 시)")
+    @Schema(description = "새 닉네임 (변경 원할 시)", example = "노고산동불주먹")
     private String nickname;
 
-    @Schema(description = "새 비밀번호 (변경 원할 시, 8자 이상)")
+    @Schema(description = "새 비밀번호 (변경 원할 시, 8자 이상)", example = "newpassword123")
     @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
     private String password;
 }
