20f52e8f9da02c2e4236a1f74f6bcff5400e3cd2
iihlolo <voidlsgml@gmail.com>
2025-11-15 21:18:49 +0900

    refactor: RestaurantSearchRequest DTO에 누락된 import 추가

--- src/main/java/com/devops3sogang/backend/dto/RestaurantSearchRequest.java
+++ src/main/java/com/devops3sogang/backend/dto/RestaurantSearchRequest.java
@@ -1,5 +1,6 @@
 package com.devops3sogang.backend.dto;
 
+import com.devops3sogang.backend.document.SortBy;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
