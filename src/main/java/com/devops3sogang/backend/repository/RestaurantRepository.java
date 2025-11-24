194c6c6e59d4f0c22c922e17096807598dc8a78b
iihlolo <voidlsgml@gmail.com>
2025-11-24 17:22:31 +0900

    불필요한 line 삭제

--- src/main/java/com/devops3sogang/backend/repository/RestaurantRepository.java
+++ src/main/java/com/devops3sogang/backend/repository/RestaurantRepository.java
@@ -3,14 +3,6 @@ package com.devops3sogang.backend.repository;
 import com.devops3sogang.backend.document.Restaurant;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-//import java.util.List;
-
 public interface RestaurantRepository extends MongoRepository<Restaurant, String>, RestaurantCustomRepository {
     boolean existsByNameAndAddress(String name, String address);
-
-// 리팩터링 후 삭제 
-//    List<Restaurant> findByIsActiveTrue();
-//    List<Restaurant> findByTypeAndIsActiveTrue(String type);
-//    List<Restaurant> findByCategoryAndIsActiveTrue(String category);
-//    List<Restaurant> findByTypeAndCategoryAndIsActiveTrue(String type, String category);
 }
