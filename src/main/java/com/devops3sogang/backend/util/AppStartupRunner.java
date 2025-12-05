71df35a406d194311f5b86eaddd8a66f9bdbe2cd
iihlolo <voidlsgml@gmail.com>
2025-12-06 08:06:09 +0900

    crawler를 직접 실행하지 않고 독립 container에서 실행하도록 변경

--- src/main/java/com/devops3sogang/backend/util/AppStartupRunner.java
+++ src/main/java/com/devops3sogang/backend/util/AppStartupRunner.java
@@ -10,14 +10,13 @@ import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
 import java.net.URI;
 import java.net.URL;
 import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
 
 @Component
 @RequiredArgsConstructor
